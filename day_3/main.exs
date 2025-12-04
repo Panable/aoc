b79f14cc1b207ccb62aeded3d04714bf5ecfc92b
Dhanveer Ramnauth <dhanr1606@gmail.com>
2025-12-04 22:56:23 +1100

    p2 done

--- day_3/main.exs
+++ day_3/main.exs
@@ -1,30 +1,47 @@
 defmodule Solve do
-  def find_max(lst, l_max, r_max, maxfd) do
-    if lst == [] do
-      IO.inspect(r_max * 10 + l_max)
-      r_max * 10 + l_max
-    else
-      cur = Enum.at(lst, 0)
-      if cur >= r_max do
-	# l_max = max l_max, r_max
-	# r_max = cur
-	find_max(List.delete_at(lst, 0), maxfd, cur, max(cur,maxfd))
-      else # cur <= r_max
-	# l_max = l_max
-	# r_max = r_max
-	find_max(List.delete_at(lst, 0), l_max, r_max, max(cur, maxfd))
-      end
-    end
+  def p2(arr, max) do
+    n = length(arr)
+    r = 12 - length(max)
+    IO.puts("n is: ")
+    IO.inspect(n)
+    IO.inspect(r)
+    IO.inspect(max)
+    #    r = n
+    cond do
+      r == n ->
+	Enum.concat(max, arr)
+      r == 0 ->
+	max
+      n > r ->
+	maxi = Enum.reduce(Enum.take(arr, n - r + 1), %{cur_idx: -1, max_idx: -1, max: -1},
+	  fn val, acc ->
+	    if val > acc.max do
+	      %{cur_idx: acc.cur_idx + 1, max_idx: acc.cur_idx + 1, max: val}
+	    else
+	      %{cur_idx: acc.cur_idx + 1, max_idx: acc.max_idx, max: acc.max}
+	    end
+	  end)
+	IO.inspect(maxi)
+	if maxi.max_idx < 0 do
+	  System.halt(0)
+	end
+	
+	p2(Enum.drop(arr, maxi.max_idx + 1), max ++ [maxi.max])
+	#IO.puts("Hello")
+	
+	#p2([1,2,3,4,5,6],[1,2,3,4,5,6])
+    end	
   end
+    
   def max_joltage(bank) do
-    #IO.inspect(bank)
-    bank = Enum.reverse(bank)
-    find_max(List.delete_at(bank, 0), Enum.at(bank, 0), 0, Enum.at(bank, 0))
+    IO.inspect(bank)
+    #Enum.take(bank, 3) |> IO.inspect()
+    p2(bank, [])
   end
 end
 
 File.read!("input.txt")
 |> String.split()
-|> Enum.map(& String.to_integer(&1) |> Integer.digits() |> Solve.max_joltage())
+|> Enum.map(& String.to_integer(&1) |> Integer.digits() |> Solve.max_joltage() |> Integer.undigits())
 |> Enum.sum()
-|> IO.puts()
+|> IO.inspect(charlists: :as_lists)
